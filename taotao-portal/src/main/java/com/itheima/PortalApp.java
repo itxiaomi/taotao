29bbc0e467bb4ff1ab6594a56183b315349cff03
itheima1 <qinyongquan@itcast.cn>
2020-12-11 10:24:52 +0800

    12月11日上午提交  1. 搭建toatao-portal 门户模块

---
+++ taotao-portal/src/main/java/com/itheima/PortalApp.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ *  @创建时间:  2020/12/11 9:13
+ *  @描述：    TODO
+ */
+@SpringBootApplication
+public class PortalApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApp.class , args);
+    }
+}
